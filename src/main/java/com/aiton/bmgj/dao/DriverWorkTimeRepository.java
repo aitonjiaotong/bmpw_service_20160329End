d1de12721dc2c1693e284a01ace647cf1e317ec0
hyq@aiton.com.cn <hyq@aiton.com.cn>
2016-03-03 13:18:28 +0800

    数据Repository接口

---
+++ src/main/java/com/aiton/bmgj/dao/DriverWorkTimeRepository.java
@@ -0,0 +1,10 @@
+package com.aiton.bmgj.dao;
+
+import com.aiton.bmgj.entity.DriverWorkTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by user on 2016/3/3.
+ */
+public interface DriverWorkTimeRepository extends JpaRepository<DriverWorkTime,Integer> {
+}
---
+++ src/main/java/com/aiton/bmgj/dao/PlanRunTimeRepository.java
@@ -0,0 +1,10 @@
+package com.aiton.bmgj.dao;
+
+import com.aiton.bmgj.entity.PlanRunTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by user on 2016/3/3.
+ */
+public interface PlanRunTimeRepository extends JpaRepository<PlanRunTime,Integer> {
+}
