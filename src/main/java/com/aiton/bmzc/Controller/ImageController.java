347723fb8430a4d2f0b1aeb5b58dc4bf60083e8c
qixuanziling@163.com <qixuanziling@163.com>
2016-03-11 15:48:49 +0800

    2016/3/7

--- src/main/java/com/aiton/bmzc/Controller/ImageController.java
+++ src/main/java/com/aiton/bmzc/Controller/ImageController.java
@@ -1,5 +1,9 @@
 package com.aiton.bmzc.Controller;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +12,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,23 +27,50 @@ public class ImageController {
     @RequestMapping("/image")
     @ResponseBody
     public void getImage(HttpServletRequest request,HttpServletResponse response) throws IOException {
-        InputStream input=request.getInputStream();
         ServletContext context=request.getServletContext();
         String str=context.getRealPath("/");
-        String valueString = null;
-        //System.out.println(request.getContextPath());
-        File file=new File("cars/car.jpg");
-        //System.out.println(file.getPath());
+        String savepath=str+"WEB-INF"+File.separator+"pages"+File.separator+"cars"+File.separator;
+        File file=new File(savepath);
         if(!file.exists()){
-            file.createNewFile();
+            file.mkdirs();
         }
-       FileOutputStream outputStream=new FileOutputStream(file);
-       int a=0;
-       while ((a=input.read())!=-1){
-            outputStream.write(a);
-       }
-       outputStream.flush();;
-       outputStream.close();
-       input.close();
+        DiskFileItemFactory fac = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(fac);
+        upload.setHeaderEncoding("utf-8");
+        List fileList = null;
+        try {
+            fileList = upload.parseRequest(request);
+        } catch (FileUploadException ex) {
+            return;
+        }
+        Iterator<FileItem> it = fileList.iterator();
+        String name="";
+        String extName="";
+        while(it.hasNext()){
+            FileItem item=it.next();
+            if(!item.isFormField()){
+                name=item.getName();
+                if(name==null||name.trim().equals("")){
+                    continue;
+                }
+                if(name.lastIndexOf(".")>=0){
+                    extName=name.substring(name.lastIndexOf("."));
+                }
+                File file1=new File(savepath+name+extName);
+                name="car";
+                if(!file1.exists()){
+                    file1.createNewFile();
+                }
+                try {
+                    item.write(file1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println(name+extName);
+        PrintWriter pw=response.getWriter();
+        pw.print(name + extName);
+        pw.close();
     }
 }
