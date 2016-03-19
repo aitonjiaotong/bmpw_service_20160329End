1f27255eeef94624a07c2013c9c35c19f8f906de
hyq@aiton.com.cn <hyq@aiton.com.cn>
2016-03-19 11:46:13 +0800

    租车静态页面跳转

---
+++ src/main/java/com/aiton/bmzc/Controller/PageController/Zc_AccountController.java
@@ -0,0 +1,15 @@
+package com.aiton.bmzc.Controller.PageController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by user on 2016/3/19.
+ */
+@Controller
+public class Zc_AccountController {
+    @RequestMapping("/toZC_Account")
+    public String execute(){
+        return "zc_account";
+    }
+}
