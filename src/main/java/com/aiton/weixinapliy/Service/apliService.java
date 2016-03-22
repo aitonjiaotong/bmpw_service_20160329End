4d70ca280282f0a63d4ea5036577f0791792a9f0
qixuanziling@163.com <qixuanziling@163.com>
2016-03-22 15:48:06 +0800

    机构租车和个人租车逻辑分离

---
+++ src/main/java/com/aiton/weixinapliy/Service/apliService.java
@@ -0,0 +1,11 @@
+package com.aiton.weixinapliy.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 16-3-22
+ * Time: 上午10:16
+ * To change this template use File | Settings | File Templates.
+ */
+public interface apliService {
+}
