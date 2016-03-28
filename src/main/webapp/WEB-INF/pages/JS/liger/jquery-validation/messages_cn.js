3fc5090a02b1d6d26a494c31eea166aa8623b1ba
hyq@aiton.com.cn <hyq@aiton.com.cn>
2016-03-28 09:19:25 +0800

    9.18

--- src/main/webapp/WEB-INF/pages/JS/liger/jquery-validation/messages_cn.js
+++ src/main/webapp/WEB-INF/pages/JS/liger/jquery-validation/messages_cn.js
@@ -20,6 +20,5 @@ jQuery.extend(jQuery.validator.messages, {
 		range: jQuery.validator.format("请输入一个介于 {0} 和 {1} 之间的值"),
 		max: jQuery.validator.format("请输入一个最大为 {0} 的值"),
 		min: jQuery.validator.format("请输入一个最小为 {0} 的值"),
-
         notnull:"不能为空"
 });
